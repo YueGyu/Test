991935484e5578e93e9553c8e79942c3e853dff4
YueGyu <dualsrjsqkd@gmail.com>
2019-05-08 21:15:07 +0900

    Update MainActivity.java
    test

--- app/src/main/java/com/example/joyeogyu/test/MainActivity.java
+++ app/src/main/java/com/example/joyeogyu/test/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.joyeogyu.test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-// Tests
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
